6b60312f8b4904926e60ef017a5197aa0fa11377
Pulkit0111 <ptyagi1551@gmail.com>
2025-03-30 23:01:24 +0530

    added a basic chat completion model using openai llm

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from faq-chatbot!")
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+model = ChatOpenAI(model = "gpt-3.5-turbo")
+
+def generate_response(user_query):
+    prompt_template = ChatPromptTemplate.from_messages([
+        ("system", "You are a helpful assistant that can answer FAQs."),
+        ("human", "{user_query}"),
+    ])
+    
+    prompt = prompt_template.invoke({"user_query": user_query})
+    response = model.invoke(prompt)
+    return response.content
+
+while True:
+    user_query = input("You: ")
+    if user_query.lower() == "exit":
+        print("Exiting...")
+        break
+    response = generate_response(user_query)
+    print("Bot: ", response)
+
